1644dcbc6e0c66f81236a6e3bfad0c4468998a05
alexangria1992 <alex040892@gmail.com>
2023-05-26 11:33:23 +1200

    Mobile Header

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+const elemToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
+
+// Navbar Variables
+
+const navbar = document.querySelector("[data-navbar]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+const navElemArr = [navCloseBtn, navOpenBtn, overlay];
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    elemToggleFunc(navbar);
+    elemToggleFunc(overlay);
+  });
+}
+
+// header scroll sticky
+const header = document.querySelector("[data-header]");
+window.addEventListener("scroll", function () {
+  if (this.window.scrollY > 10) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
